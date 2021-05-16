9cc32e349e6413a57f1515dbb151524a8e7b4f5e
surendra <surendraediga477@gmail.com>
2021-05-16 21:22:25 +0530

    update commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Draggable from "react-draggable"
+function Main(){
+  const [state ,  setState] = useState({
+    showHideModel:  false
+  })
+  const showPopup = () => {
+     setState({
+       showHideModel:true
+     })   
+  }
+  const hidePopup = () => {
+     setState({
+       showHideModel:false
+     })
+  } 
+  return( 
+    state.showHideModel ?
+    
+    <Pop>
+    <Draggable defaultPosition={{x: 350, y: -250}}>
+   
+    <div style={{backgroundColor: "cadetblue", width: "400px",
+  height: "300px"}}>
+    <h1>hello friends</h1>
+       <button onClick={hidePopup}>close popup</button>
+       </div>   
+    </Draggable>
+    </Pop>
+ :
+     <div>
 
+      <button onClick={showPopup}>show popup</button>
+     </div>
+  )
+}
+
+function Pop(props){
+  console.log(props);
+    const element = document.createElement("div")
+    useEffect(() => {
+      document.getElementById("pop").appendChild(element)
+  //   return () => {
+  //    document.getElementById("pop").removeChild(element)
+  //  }
+  }
+    )
+  return(
+    ReactDOM.createPortal(props.children,element))
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Main />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
